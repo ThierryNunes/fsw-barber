fbfb857b8ab7de8d7fc9dda28e53f70a9cd9c303
thierry <thierrynunes7@hotmail.com>
2026-03-25 16:03:18 -0400

    feat: add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,65 @@
-export default function Home() {
+import Header from "./_components/header";
+import { Input } from "./_components/ui/input";
+import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react";
+import { quickSearchOptions } from "./_constants/search";
+import Image from "next/image";
+
+const Home = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="py-4 text-blue-500">hello world</h1>
+    <div>
+      <Header />
+
+      <div className="p-5">
+        {/* INTRO */}
+        <h2 className="text-xl font-bold">Olá, Usuario bem vindo!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Buscar" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* BUSCA RAPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((item, index) => (
+            <Button key={index} className="gap-2" variant="secondary">
+              <Image
+                src={item.imageUrl}
+                alt={item.title}
+                height={16}
+                width={16}
+              />
+              {item.title}
+            </Button>
+          ))}
+        </div>
+
+        {/* BANNER */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            className="rounded-xl object-cover"
+            fill
+          />
+        </div>
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+
+        {/* POPULARES */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
