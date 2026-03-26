ebf84b1d4abc0594433372391e4c822f56f64778
thierry <thierrynunes7@hotmail.com>
2026-03-26 18:19:47 -0400

    feat: add signin with google

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
@@ -7,8 +8,12 @@ import Image from "next/image";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import SignInDialog from "./sign-in-dialog";
+import { signOut, useSession } from "next-auth/react";
 
 const SidebarSheet = () => {
+  const { data } = useSession();
+  const handleLogoutClick = () => signOut();
+
   return (
     <SheetContent>
       <SheetHeader>
@@ -16,28 +21,32 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
-        <div className="flex items-center gap-2">
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-          </Avatar>
+        {data?.user ? (
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={data?.user?.image ?? ""} />
+            </Avatar>
 
-          <div>
-            <p className="font-bold">John Doe</p>
-            <p className="text-xs">john@gmail.com</p>
+            <div>
+              <p className="font-bold">{data.user.name}</p>
+              <p className="text-xs">{data.user.email}</p>
+            </div>
           </div>
-        </div>
-
-        <h2 className="font-bold">Olá, faça seu login!</h2>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button size="icon">
-              <LogInIcon />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-[90%]">
-            <SignInDialog />
-          </DialogContent>
-        </Dialog>
+        ) : (
+          <>
+            <h2 className="font-bold">Olá, faça seu login!</h2>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="icon">
+                  <LogInIcon />
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="w-[90%]">
+                <SignInDialog />
+              </DialogContent>
+            </Dialog>
+          </>
+        )}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
@@ -79,7 +88,7 @@ const SidebarSheet = () => {
         <Button
           variant="ghost"
           className="justify-start gap-2"
-          // onClick={handleLogoutClick}
+          onClick={handleLogoutClick}
         >
           <LogOutIcon size={18} />
           Sair da conta
